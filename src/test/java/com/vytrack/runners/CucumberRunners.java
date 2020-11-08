769fd347a7704f4d911e5b361e3c45f76a930cd8
Phornsri <ponsri_Molae074@yahoo.com>
2020-11-08 18:18:29 -0500

    commit message!

--- src/test/java/com/vytrack/runners/CucumberRunners.java
+++ src/test/java/com/vytrack/runners/CucumberRunners.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@add_car_scenario_outline",
+        tags = "@smoke",
         publish = true
 )
 
